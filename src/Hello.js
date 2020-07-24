8d38b759265f7f99a39db0f36ba4846a116ebfac
shailza85 <33739384+shailza85@users.noreply.github.com>
2020-07-24 11:02:42 -0600

    Updated app.js file

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Hello extends React.Component {
+
+    render() {
+        return (
+            <>
+            <h2>My First Component</h2>
+            <p>Hello World!</p>
+            </>
+        );
+    }
+}
